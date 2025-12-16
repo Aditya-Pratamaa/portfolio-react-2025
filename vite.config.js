1f7b3685f8c712688cb51772668506981081b7ea
Aditya-Pratamaa <adityapratama@smkwikrama.sch.id>
2025-12-16 12:56:27 +0700

    fixed image

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
 
